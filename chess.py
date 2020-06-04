58b6fa82c1e51d8abef9fa698691432cd42fb8e8
kugiyasan <leobannocloutier@gmail.com>
2020-06-03 22:41:36 -0400

    basic forbidden moves done, need some collision detection

--- chess.py
+++ chess.py
@@ -29,20 +29,65 @@ class ChessGame():
 
         if initSq == destSq:
             raise GameError('You need to move the pawn')
-
-        print(initSq, destSq, self.board[initSq], chr(self.board[initSq][0]))
         
         if not chr(self.board[initSq][0]) == ('W', 'B')[self.turn]:
             raise GameError('No pawn or pawn of the adversary at initSq')
 
         if chr(self.board[destSq][0]) == ('W', 'B')[self.turn]:
-            raise GameError('There is already a pawn at destSq')
+            raise GameError('There is already one of your pawn at destSq')
+
+        if self.fordiddenMoves(self.board[initSq], initSq, destSq):
+            raise GameError('Forbidden move for this type of pawn')
 
         self.board[destSq] = self.board[initSq]
         self.board[initSq] = '00'
 
         self.turn = (self.turn + 1) % 2
+
+    def pawnLogic(self, dx, dy, pawn, initSq, destSq, side):
+        if dx == 1 and dy == side and self.board[destSq] != b'00':
+            return False
+        elif dx != 0 or self.board[destSq] != b'00':
+            return True
+        elif dy == side*2 and initSq[0] == int(-2.5*side + 3.5):
+            return False
+        elif dy == side:
+            return False
+        return True
+
+    def fordiddenMoves(self, pawn, initSq, destSq):
+        dy = destSq[0]-initSq[0]
+        dx = abs(destSq[1]-initSq[1])
+        print(pawn, dx, dy)
+
+        if pawn == b'WP':
+            return self.pawnLogic(dx, dy, pawn, initSq, destSq, -1)
+            
+        elif pawn == b'BP':
+            return self.pawnLogic(dx, dy, pawn, initSq, destSq, 1)
         
+        pawn = chr(pawn[1])
+        dy = abs(dy)
+
+        if pawn == 'R':
+            if dx != 0 and dy != 0:
+                return True
+        elif pawn == 'N':
+            if not((dx==1 and dy==2) or (dx==2 and dy==1)):
+                return True
+        elif pawn == 'B':
+            if dx != dy:
+                return True
+        elif pawn == 'Q':
+            if np.arctan(dy/dx) % (np.pi/4) != 0.0:
+                return True
+        elif pawn == 'K':
+            if not(dx < 2 and dy < 2):
+                return True
+        else:
+            raise GameError('Unknown pawn')
+
+        return False
 
     @property
     def emojiBoard(self):
@@ -51,27 +96,40 @@ class ChessGame():
         for x in range(self.BOARDSIZE):
             for y in range(self.BOARDSIZE):
                 if board[x][y] == b'00':
-                    board[x][y] = ('⬜', '⬛')[(x + y) % 2]
+                    board[x][y] = ('⬛', '⬜')[(x + y) % 2]
                     continue
                 
                 board[x][y] = board[x][y].decode("utf-8")
 
+                # board[x][y] = (board[x][y]
+                #     .replace('BB', '<:BB:717894296396890154>')
+                #     .replace('BK', '<:BK:717894296459542587>')
+                #     .replace('BN', '<:BN:717894296329781250>')
+                #     .replace('BP', '<:BP:717894296572788787>')
+                #     .replace('BQ', '<:BQ:717894296409473047>')
+                #     .replace('BR', '<:BR:717894296870584320>')
+                #     .replace('WB', '<:WB:717894297109659748>')
+                #     .replace('WK', '<:WK:717894296673452041>')
+                #     .replace('WN', '<:WN:717894296698617877>')
+                #     .replace('WP', '<:WP:717894296992219176>')
+                #     .replace('WQ', '<:WQ:717894296967315517>')
+                #     .replace('WR', '<:WR:717894296807931984>'))
 
-        return board
+        return '\n'.join(''.join(row) for row in board)
 
 class GameError(Exception):
     pass
 
-def gridprint(list2d):
-    print(*list2d, sep='\n')
-
-
 if __name__ == "__main__":
     game = ChessGame()
-    gridprint(game.emojiBoard)
+    print(game.emojiBoard)
     
-    print('White turn!')
-    game.playerMove(0, input('initSq: '), input('destSq: '))
+    while 1:
+        print('White turn!')
+        game.playerMove(0, input('initSq: '), input('destSq: '))
+        print(game.emojiBoard)
 
-    print('Black turn!')
-    game.playerMove(1, input('initSq: '), input('destSq: '))
+        print('Black turn!')
+        game.playerMove(1, input('initSq: '), input('destSq: '))
+        print(game.emojiBoard)
+    
